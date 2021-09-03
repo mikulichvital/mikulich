8311992dc313cf572360534c9034dfc8160134c3
mikulichvital <mikyla1993@gmail.com>
2021-09-04 01:48:29 +0300

    initial vommit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
 //here will by JavaScript code ...
+$(".more").toggle(function(){
+    $(this).text("less..").siblings(".complete").show();    
+}, function(){
+    $(this).text("read more..").siblings(".complete").hide();    
+});

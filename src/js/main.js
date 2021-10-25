bae392bb4584c4d77997d3eaa8946027c846ec41
mikulichvital <mikyla1993@gmail.com>
2021-10-25 13:00:24 +0300

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+  new Swiper('#swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -12,6 +12,35 @@ const swiper = new Swiper('.swiper', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+
+  new Swiper('#swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    slidesPerView: 'auto',
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 4,
+      }, 
+      1200: {
+        slidesPerView: 6,
+      }
+    },
+ 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.second_button_next',
+      prevEl: '.second_button_prev',
+      // clickable: true,
     },
   
   });

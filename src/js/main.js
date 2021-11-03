eb5d3df7af5cc67c66df264261c75e2db5dbd602
mikulichvital <mikyla1993@gmail.com>
2021-11-03 15:45:07 +0300

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 const swiper = new Swiper('.swiper', {
-
   loop: true,
-
-  // Navigation arrows
   navigation: {
     nextEl: '.slider-button-next',
     prevEl: '.slider-button-prev',
@@ -10,24 +7,18 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-
-function openMenu(evt, cityName) {
-  // Declare all variables
+function openMenu(evt, menuName) {
   var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
   tabslink = document.getElementsByClassName("tabs-link");
   for (i = 0; i < tabslink.length; i++) {
     tabslink[i].className = tabslink[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(menuName).style.display = "block";
   evt.currentTarget.className += " active";
 }

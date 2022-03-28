a6020c972012e68a460bf99c691f9a8929b1105a
mikulichvital <mikyla1993@gmail.com>
2022-03-28 18:06:21 +0300

    start project

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,19 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  navigation: {
-    nextEl: '.slider-button-next',
-    prevEl: '.slider-button-prev',
-  },
-
-});
-
 function openMenu(evt, menuName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    tabslink = document.getElementsByClassName("tabs-link");
+    for (i = 0; i < tabslink.length; i++) {
+      tabslink[i].className = tabslink[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-
-  tabslink = document.getElementsByClassName("tabs-link");
-  for (i = 0; i < tabslink.length; i++) {
-    tabslink[i].className = tabslink[i].className.replace(" active", "");
-  }
-
-  document.getElementById(menuName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-document.getElementById('btn-category-popular').onclick = function (event) { openMenu(event, 'popular') };
-document.getElementById('btn-category-vegetables').onclick = function (event) { openMenu(event, 'vegetables') };
-document.getElementById('btn-category-meat').onclick = function (event) { openMenu(event, 'meat') };
+  
+  document.getElementById('btn-category-popular').onclick = function (event) { openMenu(event, 'popular') };
+  document.getElementById('btn-category-vegetables').onclick = function (event) { openMenu(event, 'vegetables') };
+  document.getElementById('btn-category-meat').onclick = function (event) { openMenu(event, 'meat') };

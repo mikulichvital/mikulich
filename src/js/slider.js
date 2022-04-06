f2302d4ea1fe626ef1a437b10c0b5a6ea6e25a54
mikulichvital <mikyla1993@gmail.com>
2022-04-06 09:48:25 +0300

    commit

---
+++ src/js/slider.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+});
+
+function openMenu(evt, menuName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  tabslink = document.getElementsByClassName("tabs-link");
+  for (i = 0; i < tabslink.length; i++) {
+    tabslink[i].className = tabslink[i].className.replace(" active", "");
+  }
+
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById('btn-category-popular').onclick = function (event) { openMenu(event, 'popular') };
+document.getElementById('btn-category-vegetables').onclick = function (event) { openMenu(event, 'vegetables') };
+document.getElementById('btn-category-meat').onclick = function (event) { openMenu(event, 'meat') };
